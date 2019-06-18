b12525d2f5f2bd2235d15e596208357f4014ecb1
tavisca-agupta <agupta@tavisca.in>
2019-06-19 01:25:56 +0530

    Added author and email

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main()
 {
+    //Author :aman
     int a=10;       //default value of a is 10
     printf("Hello world\n");
     printf("This is modified commit with value %d\n",a);
