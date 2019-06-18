b73d0bca5fcbb0004bda33aafdac02be0345042e
tavisca-agupta <agupta@tavisca.in>
2019-06-18 23:23:13 +0530

    added a default value variable

--- hello.c
+++ hello.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main()
 {
+    int a=10;
     printf("Hello world\n");
-    printf("This is modified commit\n");
+    printf("This is modified commit with value %d\n",a);
     return 0;
 }
