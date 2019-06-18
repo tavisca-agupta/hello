7add2bef72ddd6ce750ef9faf2022abe066f76cc
tavisca-agupta <agupta@tavisca.in>
2019-06-18 23:24:15 +0530

    added a comment

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int a=10;
+    int a=10;       //default value of a is 10
     printf("Hello world\n");
     printf("This is modified commit with value %d\n",a);
     return 0;
